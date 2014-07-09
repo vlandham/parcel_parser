3698741ec27378f9586d4b47484eb48fc449617d
Jim V <vlandham@gmail.com>
2014-07-09 07:50:40 -0700

    fix

--- elastic/simple_query.js
+++ elastic/simple_query.js
@@ -1,22 +1,34 @@
 var elasticsearch = require('elasticsearch');
 var q = require('q');
 
+// localhost:9200/_search
+// {
+//     "query": {
+//         "query_string": {
+//             "query": "RYAN",
+//           "fields":["Name"]
+//         }
+//     }
+// }
+// 
 
 var client = new elasticsearch.Client({
   host: '127.0.0.1:9200'
   // log: 'trace'
 });
 
-client.search({
-  "query": {
-    "query_string": {
-      "query": "xxxdsd",
-      "fields":["Name"]
+var query = {
+  body: {
+    query: {
+      "query_string": {
+        "query": "RYAN",
+        "fields":["Name"]
+      }
     }
   }
-  
-  
-}).then(function(body) {
+}
+
+client.search(query).then(function(body) {
   var hits = body.hits.hits;
   console.log(body);
 }).done();
